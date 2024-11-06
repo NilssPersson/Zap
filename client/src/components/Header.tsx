bdf7857d3830db6d913deb91db5f72e2a2ebb89f
FKnorring <fille.vk@gmail.com>
2024-11-06 16:30:11 +0100

    Added display font

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -6,12 +6,12 @@ export function Header() {
     <header className="border-b">
       <div className="container flex h-16 items-center px-4">
         <div className="mr-4 hidden md:flex">
-          <nav className="flex items-center space-x-6 text-sm font-medium">
+          <nav className="flex items-center space-x-6 font-medium font-display">
             <Link to="/">
-              <Button variant="ghost">Home</Button>
+              <Button variant="ghost" className="text-lg">Home</Button>
             </Link>
             <Link to="/about">
-              <Button variant="ghost">About</Button>
+              <Button variant="ghost" className="text-lg">About</Button>
             </Link>
           </nav>
         </div>
