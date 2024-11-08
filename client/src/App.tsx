ff7f06dd6804799694ecd7f10cb6597db13b3c26
FKnorring <fille.vk@gmail.com>
2024-11-08 09:47:08 +0100

    Authentication added

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,17 +1,26 @@
 import { Routes, Route } from 'react-router-dom'
 import { Header } from './components/Header'
+import { KindeProvider } from '@kinde-oss/kinde-auth-react'
 import About from './pages/About'
 import Home from './pages/Home'
 
 function App() {
   return (
     <div className="min-h-screen bg-[url('/assets/bg-mobile.svg')] md:bg-[url('/assets/bg-desktop.svg')] bg-cover bg-no-repeat bg-center">
-      <Header />
       <main>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-        </Routes>
+        <KindeProvider
+          clientId="7b50afa95aec4cba88bad0abb4b94dfa"
+          domain="https://gameshack.kinde.com"
+          redirectUri="http://localhost:5173"
+          logoutUri="http://localhost:5173"
+        >
+          <Header />
+
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+          </Routes>
+        </KindeProvider>
       </main>
     </div>
   )
--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,11 +1,13 @@
 import { Button } from "@/components/ui/button"
+import { useKindeAuth } from "@kinde-oss/kinde-auth-react"
 import { HousePlug } from "lucide-react"
 import { Link } from "react-router-dom"
 
 export function Header() {
+  const { login, logout, register, isAuthenticated } = useKindeAuth() 
   return (
-    <header className="border-b">
-      <div className="container flex h-16 items-center px-4">
+    <header className="border-b bg-background">
+      <div className="container flex h-16 items-center px-4 bg-background">
         <div className="mr-4 hidden md:flex w-full">
           <nav className="flex items-center space-x-6 font-medium font-display content-between">
             <Link to="/" className="flex items-center gap-2 text-2xl mr-8">
@@ -19,6 +21,16 @@ export function Header() {
               <Link to="/about">
                 <Button variant="ghost" className="text-lg">About</Button>
               </Link>
+            </div>
+            <div>
+              {isAuthenticated ? (
+                <Button onClick={logout} type="button">Logout</Button>
+              ) : (
+                <>
+                  <Button onClick={() => register()} type="button" variant="outline">Register</Button>
+                  <Button onClick={() => login()} type="button">Login</Button>
+                </>
+              )}
             </div>
           </nav>
         </div>
