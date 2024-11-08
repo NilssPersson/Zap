336a9f22c0cd035fd9759cc3eae68bea66f666b9
Nils <nils.albin.persson@gmail.com>
2024-11-08 10:35:23 +0100

    Testing the realtime communication

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,8 @@
-import { Routes, Route } from 'react-router-dom'
-import { Header } from './components/Header'
-import About from './pages/About'
-import Home from './pages/Home'
+import { Routes, Route } from "react-router-dom";
+import { Header } from "./components/Header";
+import About from "./pages/About";
+import Home from "./pages/Home";
+import RoomTest from "./pages/RoomTest";
 
 function App() {
   return (
@@ -11,10 +12,11 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
+          <Route path="/room-test" element={<RoomTest />} />
         </Routes>
       </main>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
