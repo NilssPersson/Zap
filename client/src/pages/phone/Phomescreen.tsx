e0a446340e7a0aec82886531010a552a1ebecb9a
Jacob DIllström <78909969+Dillenzz@users.noreply.github.com>
2024-11-08 11:07:53 +0100

    Phomeview test

--- client/src/pages/phone/Phomescreen.tsx
+++ client/src/pages/phone/Phomescreen.tsx
@@ -5,12 +5,12 @@ function Phomescreen() {
     return (
       <div className="h-screen flex flex-col">
         {/* Top section with the heading */}
-        <header className="flex items-center justify-center py-4"> {/* Reduced padding here */}
+        <header className="flex items-center justify-center py-4"> 
           <h1 className="text-7xl font-bold font-display">GameShack</h1>
         </header>
   
         {/* Centered Input and Button section */}
-        <div className="flex flex-col items-center justify-start mt-12 w-1/2 mx-auto space-y-8"> {/* Changed flex-grow to mt-12 */}
+        <div className="flex flex-col items-center justify-start mt-12 w-1/2 mx-auto space-y-8"> 
           <h2 className="text-4xl font-bold font-display">Enter a room!</h2>
           <Input
             type="email"
