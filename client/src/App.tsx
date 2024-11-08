e2c4855a01ce7de60f1729541e1d030581c55f28
FKnorring <fille.vk@gmail.com>
2024-11-08 10:46:32 +0100

    Added base api functionality

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,15 +3,16 @@ import { Header } from './components/Header'
 import { KindeProvider } from '@kinde-oss/kinde-auth-react'
 import About from './pages/About'
 import Home from './pages/Home'
+import Builder from './pages/Builder'
 
 function App() {
   return (
     <div className="min-h-screen bg-[url('/assets/bg-mobile.svg')] md:bg-[url('/assets/bg-desktop.svg')] bg-cover bg-no-repeat bg-center">
-      <main>
+      <main className='min-h-screen flex flex-col'>
         <KindeProvider
           clientId="7b50afa95aec4cba88bad0abb4b94dfa"
           domain="https://gameshack.kinde.com"
-          redirectUri="http://localhost:5173"
+          redirectUri="http://localhost:5173/builder"
           logoutUri="http://localhost:5173"
         >
           <Header />
@@ -19,6 +20,7 @@ function App() {
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/about" element={<About />} />
+            <Route path="/builder" element={<Builder />} />
           </Routes>
         </KindeProvider>
       </main>
---
+++ client/src/models/Game.ts
@@ -0,0 +1,9 @@
+interface Game {
+    id: string;
+    name: string;
+    createdAt: string;
+    updatedAt: string;
+    userId: string;
+}
+
+export default Game;
