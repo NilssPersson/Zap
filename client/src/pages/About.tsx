5c2ea32e5abc6db057a2f2c4a273f030c7a8f9a3
Lisa Hansson <lisa.ia@hotmail.com>
2024-11-08 11:27:29 +0100

    Updated font and pictures in about us

--- client/src/pages/About.tsx
+++ client/src/pages/About.tsx
@@ -7,20 +7,22 @@ import filip from "../assets/photos/filip.jpeg";
 function About() {
   return (
     <div className="container px-4 py-6">
-      <h1 className="text-2xl font-bold mb-4">About Us</h1>
-      <p className="text-white-600 mb-4">
+      <h1 className="text-2xl font-bold mb-4 font-display">About Us</h1>
+      <p className="text-white-600 mb-4 font-display">
         Welcome to our about page. This is where you can learn more about our
         company and mission.
       </p>
       <h3 className="text-2xl font-bold mb-4">Who are we?</h3>
-      <div className="grid grid-cols-3 mb-6">
+      <div className="flex flex-row justify-between">
         <div className="flex flex-col items-center">
           <img
             src={lisa}
             alt="Lisa Hansson"
             className="w-24 h-24 rounded-full shadow-lg mb-2"
           />
-          <p className="text-center text-white-700">Lisa Hansson</p>
+          <p className="text-center text-white-700 font-display">
+            Lisa Hansson
+          </p>
         </div>
         <div className="flex flex-col items-center">
           <img
@@ -28,7 +30,9 @@ function About() {
             alt="Nils Persson"
             className="w-24 h-24 rounded-full shadow-lg mb-2"
           />
-          <p className="text-center text-white-700">Nils Persson</p>
+          <p className="text-center text-white-700 font-display">
+            Nils Persson
+          </p>
         </div>
         <div className="flex flex-col items-center">
           <img
@@ -36,17 +40,21 @@ function About() {
             alt="Ramez Shakarna"
             className="w-24 h-24 rounded-full shadow-lg mb-2"
           />
-          <p className="text-center text-white-700">Ramez Shakarna</p>
+          <p className="text-center text-white-700 font-display">
+            Ramez Shakarna
+          </p>
         </div>
       </div>
-      <div className="grid grid-cols-2 mb-6">
+      <div className="flex flex-row justify-around">
         <div className="flex flex-col items-center">
           <img
             src={filip}
             alt="Filip von Knorring"
             className="w-24 h-24 rounded-full shadow-lg mb-2"
           />
-          <p className="text-center text-white-700">Filip von Knorring</p>
+          <p className="text-center text-white-700 font-display">
+            Filip von Knorring
+          </p>
         </div>
         <div className="flex flex-col items-center">
           <img
@@ -54,26 +62,30 @@ function About() {
             alt="Jacob Dillström"
             className="w-24 h-24 rounded-full shadow-lg mb-2"
           />
-          <p className="text-center text-white-700">Jacob Dillström</p>
+          <p className="text-center text-white-700 font-display">
+            Jacob Dillström
+          </p>
         </div>
       </div>
-      <h3 className="text-2xl font-bold mb-4">Goal of the project</h3>
-      <p className="text-white-600 mb-4">
+      <h3 className="text-2xl font-bold mb-4 font-display">
+        Goal of the project
+      </h3>
+      <p className="text-white-600 mb-4 font-display">
         We’re a team of students from Uppsala University, here to bring you a
         fun, easy way to create interactive quizzes! Born out of our Software
         Engineering Project course, our mission is simple: make quiz-making a
         blast for everyone. Dive into our ready-to-go templates and create
         something awesome in minutes!
       </p>
-      <h3 className="text-2xl font-bold mb-4">Current status</h3>
-      <p className="text-white-600 mb-4">
+      <h3 className="text-2xl font-bold mb-4 font-display">Current status</h3>
+      <p className="text-white-600 mb-6 font-display">
         The project is currently in its start-up phase, to be continued...
       </p>
       <a
         href="https://github.com/FKnorring/GameShack"
         target="_blank"
         rel="noopener noreferrer"
-        className="text-2xl font-bold mb-8 hover:text-green-700"
+        className="text-3xl font-display mb-8 hover:text-blue-900"
       >
         Check out our GitHub Repository!
       </a>
