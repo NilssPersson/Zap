c136c26523f54dc8713e5f3bdd92a80d3f8aafd7
Jacob DIllström <78909969+Dillenzz@users.noreply.github.com>
2024-11-08 14:42:20 +0100

    Display Finally works

--- client/src/pages/phone/Phomescreen.tsx
+++ client/src/pages/phone/Phomescreen.tsx
@@ -2,25 +2,23 @@ import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 
 function Phomescreen() {
-
-    
-  
-       
   return (
-    <div className="flex-1 flex flex-col items-center justify-center">
-      {/* Header above the form */}
-      <header className="flex items-center justify-center py-4">
-        <h1  className="text-7xl font-bold font-display mb-16 fancy-wrap">GameShack</h1>
-      </header>
+    <div className="flex-1 w-full flex items-center justify-center overflow-hidden p-8">
+      {/* Centered Content */}
+      <div className="flex flex-col items-center justify-center w-full max-w-md space-y-8 bg-[#fefefe] rounded-2xl p-8 shadow-lg">
+        <header className="text-center">
+          <h1 className="font-bold font-display text-[#333333] text-5xl sm:text-5xl md:text-5xl lg:text-5xl xl:text-6xl">
+            GameShack
+          </h1>
+        </header>
 
-      {/* Centered Input and Button section */}
-      <div className="bg-[#fefefe] rounded-2xl flex flex-col items-center justify-start w-3/5 space-y-8 py-12 px-8 shadow-lg ">
+        {/* Input and Button section */}
         <Input
           placeholder="Code"
-          className="text-center border-gray-400 font-display rounded-md text-3xl py-4 px-6 w-full shadow-lg"
+          className="text-black text-center border-gray-300 rounded-md font-display text-3xl py-8 px-12 w-full shadow-lg"
         />
 
-        <Button className="bg-[#333333] text-3xl text-[#fefefe] font-display hover:bg-[#86D293] py-4 px-6 w-full shadow-lg">
+        <Button className="bg-[#333333] text-3xl text-[#fefefe] hover:bg-[#86D293] py-8 px-12 font-display w-full shadow-lg">
           Join
         </Button>
       </div>
