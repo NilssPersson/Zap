38ddf61350f193020b7e21afd584e1e96eceaf77
Nils <nils.albin.persson@gmail.com>
2024-11-14 13:30:49 +0100

    firebase init

---
+++ client/src/services/client.ts
@@ -0,0 +1,28 @@
+import { useState, useEffect } from "react";
+import { ref, onValue, off } from "firebase/database";
+import { database } from "@/firebase";
+
+export const useGameStatus = (quizCode:string) => {
+  const [questionNumber, setQuestionNumber] = useState(null);
+
+  useEffect(() => {
+    const quizRef = ref(database, `ongoingQuiz/${quizCode}`);
+
+    const handleQuizChange = (snapshot:any) => {
+      if (snapshot.exists()) {
+        const gameData = snapshot.val();
+        setQuestionNumber(gameData.questionNumber);
+      } else {
+        console.error("Game not found");
+      }
+    };
+
+    onValue(quizRef, handleQuizChange);
+
+    return () => {
+      off(quizRef, "value", handleQuizChange);
+    };
+  }, [quizCode]);
+
+  return { hasAnswered, questionNumber, setHasAnswered };
+};
