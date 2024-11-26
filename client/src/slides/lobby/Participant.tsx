704dc013a872be14cfcd6dd6f96b2e9a653214a4
Nils <nils.albin.persson@gmail.com>
2024-11-26 14:43:32 +0100

    Answer slide for FA questions

---
+++ client/src/slides/lobby/Participant.tsx
@@ -0,0 +1,11 @@
+import { LobbySlide } from "@/models/Quiz";
+
+export function Participant({ slide }: { slide: LobbySlide }) {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <h1 className="text-3xl font-display text-center">
+        Waiting for the host to start the game... {slide.title}
+      </h1>
+    </div>
+  );
+}
--- client/src/slides/question/fa/HostAnswer.tsx
+++ client/src/slides/question/fa/HostAnswer.tsx
@@ -1,18 +1,40 @@
 import { FASlide } from "@/models/Quiz";
-import { Preview } from "./Preview";
 import { Button } from "@/components/ui/button";
+import { Participant } from "@/models/Quiz";
 
 export function HostAnswer({
   slide,
   onNextSlide,
+  participants = [],
 }: {
   slide: FASlide;
   onNextSlide: () => void;
+  participants: Participant[];
 }) {
+  const latestAnswers =
+    participants.length > 0
+      ? participants.map((participant) => {
+          return participant.answers[participant.answers.length - 1];
+        })
+      : [];
+
   return (
-    <div className="flex flex-col">
-      <Preview slide={slide} />
-      <h1>Showing answer...</h1>
+    <div className="flex flex-col items-center">
+      <div className="bg-white rounded p-4 mb-10">
+        <h1 className="text-4xl text-black font-display">{slide.title}</h1>
+      </div>
+
+      <div className="flex flex-row space-x-2 ">
+        {latestAnswers.map((answer) => {
+          return (
+            <div className="bg-white rounded p-2 flex flex-row">
+              <h1 className="text-xl text-black font-display">
+                {answer.answer}
+              </h1>
+            </div>
+          );
+        })}
+      </div>
       <Button
         onClick={() => {
           onNextSlide();
