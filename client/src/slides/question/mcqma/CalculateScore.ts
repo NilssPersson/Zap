1983aa63498b9c0146af344ecba16c1f8688c9a3
Lisa Hansson <lisa.ia@hotmail.com>
2024-12-10 15:15:57 +0100

    Fixed calculate score logic for MCQMA

--- client/src/slides/question/mcqma/CalculateScore.ts
+++ client/src/slides/question/mcqma/CalculateScore.ts
@@ -1,6 +1,6 @@
-import { MCQMASlide } from "@/models/types/questions";
+import { MCQMASlide } from '@/models/types/questions';
 
-import { CalculateScoreProps } from "@/slides";
+import { CalculateScoreProps } from '@/slides';
 
 export function CalculateScore({
   slide,
@@ -9,7 +9,10 @@ export function CalculateScore({
   const { options } = slide;
   return participants.map((participant) => {
     const latestAnswer = participant.answers?.at(-1)?.answer;
-    if (!latestAnswer) return 0;
-    return options.filter((option) => option.isCorrect).every((option) => latestAnswer.includes(option.text)) ? slide.points : 0;
+    const correctOptions = options.filter((option) => option.isCorrect);
+    if (!latestAnswer || correctOptions.length != latestAnswer.length) return 0;
+    return correctOptions.every((option) => latestAnswer.includes(option.text))
+      ? slide.points
+      : 0;
   });
 }
--- client/src/slides/question/mcqma/Host.tsx
+++ client/src/slides/question/mcqma/Host.tsx
@@ -18,8 +18,8 @@ export function Host({
       <div className="flex flex-col items-center justify-center p-10">
         <div
           className={cn(
-            'grid grid-cols-2 gap-14',
-            slide.options.length > 4 && 'grid grid-cols-3 gap-14'
+            'grid gap-14',
+            slide.options.length > 4 ? 'grid-cols-3' : 'grid-cols-2'
           )}
         >
           {/* Only display 4 options, without any click functionality */}
@@ -29,7 +29,7 @@ export function Host({
               style={{
                 backgroundColor: getColor(index), // Get different color for each div. Centrerad text och bredare
               }}
-              className="flex items-center justify-center text-5xl text-white font-display h-64 w-[550px] rounded-lg box-border p-8" // Same padding and box-border as Preview
+              className="flex items-center justify-center text-5xl text-white font-display h-64 w-[550px] rounded-lg box-border p-8"
             >
               <span>{option.text}</span>
             </div>
