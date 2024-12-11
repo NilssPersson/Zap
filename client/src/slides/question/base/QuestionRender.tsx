d5123cba8409f142204f5b74c2c892af93480cb8
Lisa Hansson <lisa.ia@hotmail.com>
2024-12-11 15:21:06 +0100

    Fixed placement of text and participant answer in base question

--- client/src/slides/question/base/QuestionRender.tsx
+++ client/src/slides/question/base/QuestionRender.tsx
@@ -1,7 +1,7 @@
-import { Participant, QuestionSlide } from "@/models/Quiz";
-import { SlideTitle } from "@/slides/_components/SlideTitle";
-import { SlideContent } from "@/slides/_components/SlideContent";
-import { ParticipantAnswers } from "@/slides/_components/ParticipantAnswers";
+import { Participant, QuestionSlide } from '@/models/Quiz';
+import { SlideTitle } from '@/slides/_components/SlideTitle';
+import { SlideContent } from '@/slides/_components/SlideContent';
+import { ParticipantAnswers } from '@/slides/_components/ParticipantAnswers';
 export function BaseQuestionRender({
   slide,
   children,
@@ -15,7 +15,13 @@ export function BaseQuestionRender({
     <div className="flex-1 flex flex-col space-y-12 w-full justify-center">
       <div className="flex flex-row justify-center items-center">
         <div className="space-y-8">
-          <SlideTitle title={slide.title} />
+          <div className="flex flex-row justify-center p-40 ">
+            <SlideTitle title={slide.title} />
+            <div className="mr-1">
+              <ParticipantAnswers participants={participants} />
+            </div>
+          </div>
+
           {slide.imageUrl && (
             <div className="flex justify-center">
               <div className="relative flex items-center justify-center">
@@ -25,16 +31,13 @@ export function BaseQuestionRender({
                   className="w-auto object-contain"
                   style={{
                     height: `${(slide.imageScale || 1) * 400}px`,
-                    transition: "height 0.2s ease-out",
+                    transition: 'height 0.2s ease-out',
                   }}
                 />
               </div>
             </div>
           )}
           <SlideContent content={slide.content} />
-        </div>
-        <div className="absolute right-10">
-          <ParticipantAnswers participants={participants} />
         </div>
       </div>
       {children}
