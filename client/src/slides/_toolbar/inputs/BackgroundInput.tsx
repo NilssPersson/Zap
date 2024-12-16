d4f174b6455c06d769e4a390f889cf2cb00c53b6
Lisa Hansson <lisa.ia@hotmail.com>
2024-12-16 13:10:57 +0100

    Some translations

--- client/src/slides/_toolbar/inputs/BackgroundInput.tsx
+++ client/src/slides/_toolbar/inputs/BackgroundInput.tsx
@@ -1,32 +1,35 @@
-import { BackgroundStyle } from "@/components/quiz-editor/QuizBackground";
+import { BackgroundStyle } from '@/components/quiz-editor/QuizBackground';
 import {
   SelectItem,
   SelectTrigger,
   SelectValue,
   SelectContent,
-} from "@/components/ui/select";
-import { Label } from "@/components/ui/label";
-import { Select } from "@/components/ui/select";
-import { ToolbarProps } from "../../toolbar";
-import { Slide } from "@/models/Quiz";
-import { Palette } from "lucide-react";
+} from '@/components/ui/select';
+import { Label } from '@/components/ui/label';
+import { Select } from '@/components/ui/select';
+import { ToolbarProps } from '../../toolbar';
+import { Slide } from '@/models/Quiz';
+import { Palette } from 'lucide-react';
+import { useTranslation } from 'react-i18next';
 
 export default function BackgroundInput<T extends Slide>({
   slide,
   onSlideUpdate,
 }: ToolbarProps<T>) {
   const backgroundStyles: { value: BackgroundStyle; label: string }[] = [
-    { value: BackgroundStyle.Waves, label: "Waves" },
-    { value: BackgroundStyle.Blob, label: "Blob" },
-    { value: BackgroundStyle.BlobInverted, label: "Inverted Blob" },
-    { value: BackgroundStyle.Circle, label: "Circles" },
+    { value: BackgroundStyle.Waves, label: 'Waves' },
+    { value: BackgroundStyle.Blob, label: 'Blob' },
+    { value: BackgroundStyle.BlobInverted, label: 'Inverted Blob' },
+    { value: BackgroundStyle.Circle, label: 'Circles' },
   ];
+
+  const { t } = useTranslation(['quizEditor']);
 
   return (
     <div className="space-y-1">
       <div className="flex flex-row items-center space-x-1">
         <Palette size={17} />
-        <Label>Background Style</Label>
+        <Label>{t('backgroundStyle')}</Label>
       </div>
       <Select
         value={slide.backgroundStyle || BackgroundStyle.Waves}
