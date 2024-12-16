9b423d29c8cc76a5c0754e938c09b6542360c928
Lisa Hansson <lisa.ia@hotmail.com>
2024-12-16 10:46:36 +0100

    Added preview to FA

--- client/src/slides/question/rank/index.ts
+++ client/src/slides/question/rank/index.ts
@@ -2,22 +2,22 @@ export * from './Preview';
 export * from './Participant';
 export * from './ParticipantAnswer';
 export * from './Host';
-export * from './HostAnswer';   
+export * from './HostAnswer';
 export * from './CalculateScore';
 
 import { QuestionTypes, SlideTypes, AnswerTypes } from '@/models/Quiz';
-import { ListOrdered } from "lucide-react";
+import { ListOrdered } from 'lucide-react';
 import { SlideInfo } from '../..';
 
 export const Info: SlideInfo = {
-    value: "question:RANK",
-    icon: ListOrdered,
-    label: "Rank It Right",
-    slideType: SlideTypes.question,
-    questionType: QuestionTypes.RANK,
-    defaults: {
-        ranking: ["Best", "Good", "Bad", "Worst"],
-        answerType: AnswerTypes.rank,
-        points:1000,
-    }
-} as const; 
+  value: 'question:RANK',
+  icon: ListOrdered,
+  label: 'Rank It Right',
+  slideType: SlideTypes.question,
+  questionType: QuestionTypes.RANK,
+  defaults: {
+    ranking: ['First', 'Second', 'Third', 'Last'],
+    answerType: AnswerTypes.rank,
+    points: 1000,
+  },
+} as const;
