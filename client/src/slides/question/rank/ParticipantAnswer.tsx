8f6f0f8c543777c24f1d0aecae5a35a974252090
Lisa Hansson <lisa.ia@hotmail.com>
2024-12-17 11:22:44 +0100

    Show participants ranking in answer

--- client/src/slides/question/rank/ParticipantAnswer.tsx
+++ client/src/slides/question/rank/ParticipantAnswer.tsx
@@ -1,10 +1,37 @@
-import { Participant } from "@/models/Quiz";
-import RenderCorrectIncorrect from "../base/RenderCorrectIncorrect";
+import { Participant, RankSlide } from '@/models/Quiz';
+import RenderCorrectIncorrect from '../base/RenderCorrectIncorrect';
 
 export function ParticipantAnswer({
+  slide,
   participant,
 }: {
+  slide: RankSlide;
   participant: Participant;
 }) {
-  return <RenderCorrectIncorrect participant={participant} />;
+  const correctAnswer = slide.ranking;
+  const latestAnswer = participant.answers?.at(-1)?.answer;
+
+  if (latestAnswer) {
+    return (
+      <div>
+        <div className="flex flex-col items-center justify-center m-10 gap-2">
+          {latestAnswer.map((text, index) => {
+            const bgColor =
+              text == correctAnswer[index] ? 'bg-green-500' : 'bg-red-500';
+            return (
+              <div key={index} className="flex items-center w-full space-x-4">
+                <div
+                  className={`flex items-center w-full p-4 rounded-lg text-4xl font-display text-white ${bgColor}`}
+                >
+                  <span>{text}</span>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    );
+  } else {
+    return <RenderCorrectIncorrect participant={participant} />;
+  }
 }
