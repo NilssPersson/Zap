8b2c7c889e75878dc2792fcf646be1e91e35d5f5
Nils <nils.albin.persson@gmail.com>
2024-12-18 10:11:31 +0100

    New profile UI

--- client/src/components/Settings.tsx
+++ client/src/components/Settings.tsx
@@ -12,16 +12,24 @@ import {
 } from '@/components/ui/collapsible';
 import { useTranslation } from 'react-i18next';
 import Profile from '@/pages/User/Profile';
-import { ChevronDown, LogIn } from 'lucide-react';
+import {
+  ChevronDown,
+  CircleUserRound,
+  Languages,
+  LogIn,
+  MessageCircleQuestion,
+  UserPen,
+} from 'lucide-react';
 import { useKindeAuth } from '@kinde-oss/kinde-auth-react';
-import { Separator } from '@radix-ui/react-dropdown-menu';
 import { RadioGroup, RadioGroupItem } from '@/components/ui/radio-group';
 import { Label } from '@/components/ui/label';
 import { Switch } from '@/components/ui/switch';
 import { useAppContext } from '@/contexts/App/context';
+import { Separator } from '@/components/ui/separator';
 
 export default function Settings() {
   const { t, i18n } = useTranslation();
+  const [isSettingsOpen, setIsSettingsOpen] = useState(false);
   const [isLanguageOpen, setIsLanguageOpen] = useState(false);
   const [isAppearanceOpen, setIsAppearanceOpen] = useState(false);
   const [isTutorialOpen, setIsTutorialOpen] = useState(false);
@@ -53,25 +61,46 @@ export default function Settings() {
   };
 
   return (
-    <Popover>
+    <Popover open={isSettingsOpen} onOpenChange={setIsSettingsOpen}>
       <PopoverTrigger asChild>
         <Button variant="ghost" className="text-lg flex flex-row items-center">
           {t('general:profile')}
+          <ChevronDown
+            className="w-4 h-4"
+            strokeWidth={3}
+            style={{
+              transition: 'transform 0.2s ease',
+              transform: isSettingsOpen ? `rotate(180deg)` : 'none',
+            }}
+          />
         </Button>
       </PopoverTrigger>
       <PopoverContent className="w-[220px] p-3 ">
         <div className="grid gap-2">
+          {isAuthenticated && (
+            <>
+              <div className="flex items-center space-x-2 w-[200px]">
+                <CircleUserRound className="w-6 h-6" />
+                <p className="font-display truncate text-sm">{user?.email}</p>
+              </div>
+              <Separator className=" w-full" />
+            </>
+          )}
           <Collapsible open={isLanguageOpen} onOpenChange={setIsLanguageOpen}>
-            <CollapsibleTrigger className="flex flex-row w-full items-center text-left px-2 py-2 font-display hover:bg-primary/90 rounded">
+            <CollapsibleTrigger className="flex flex-row w-full items-center px-2 py-2 font-display hover:bg-primary/90 rounded justify-between">
+              <div className="flex items-center">
+                <Languages className="w-4 h-4 mr-2" strokeWidth={3} />
+                {t('general:language')}
+              </div>
+
               <ChevronDown
-                className="w-4 h-4 mr-2"
+                className="w-4 h-4"
                 strokeWidth={3}
                 style={{
                   transition: 'transform 0.2s ease',
-                  transform: isLanguageOpen ? `rotate(180deg)` : 'none',
+                  transform: isLanguageOpen ? 'rotate(180deg)' : 'none',
                 }}
               />
-              {t('general:language')}
             </CollapsibleTrigger>
 
             <CollapsibleContent className="px-4 py-3 rounded bg-gray-200">
@@ -107,16 +136,20 @@ export default function Settings() {
                 open={isAppearanceOpen}
                 onOpenChange={setIsAppearanceOpen}
               >
-                <CollapsibleTrigger className="flex flex-row w-full items-center text-left px-2 py-2 font-display hover:bg-primary/90 rounded">
+                <CollapsibleTrigger className="flex flex-row w-full items-center px-2 py-2 font-display hover:bg-primary/90 rounded justify-between">
+                  <div className="flex items-center">
+                    <UserPen className="w-4 h-4 mr-2" strokeWidth={3} />
+                    {t('general:appearance')}
+                  </div>
+
                   <ChevronDown
-                    className="w-4 h-4 ml-0 mr-2"
+                    className="w-4 h-4"
                     strokeWidth={3}
                     style={{
                       transition: 'transform 0.2s ease',
-                      transform: isAppearanceOpen ? `rotate(180deg)` : 'none',
+                      transform: isAppearanceOpen ? 'rotate(180deg)' : 'none',
                     }}
                   />
-                  {t('general:appearance')}
                 </CollapsibleTrigger>
                 <CollapsibleContent className="px-2 py-2 bg-gray-200 rounded">
                   <div className="grid gap-2">
@@ -129,21 +162,30 @@ export default function Settings() {
                 open={isTutorialOpen}
                 onOpenChange={setIsTutorialOpen}
               >
-                <CollapsibleTrigger className="flex flex-row w-full items-center text-left px-2 py-2 font-display hover:bg-primary/90 rounded">
+                <CollapsibleTrigger className="flex flex-row w-full items-center px-2 py-2 font-display hover:bg-primary/90 rounded justify-between">
+                  <div className="flex items-center">
+                    <MessageCircleQuestion
+                      className="w-4 h-4 mr-2"
+                      strokeWidth={3}
+                    />
+                    {t('general:tutorial')}
+                  </div>
+
                   <ChevronDown
-                    className="w-4 h-4 ml-0 mr-2"
+                    className="w-4 h-4"
                     strokeWidth={3}
                     style={{
                       transition: 'transform 0.2s ease',
-                      transform: isTutorialOpen ? `rotate(180deg)` : 'none',
+                      transform: isTutorialOpen ? 'rotate(180deg)' : 'none',
                     }}
                   />
-                  Tutorials
                 </CollapsibleTrigger>
                 <CollapsibleContent className="px-4 py-3 rounded bg-gray-200">
                   <div className="grid gap-4">
                     <div className="flex items-center justify-between">
-                      <Label htmlFor="tutorial-toggle">Enable Tutorials</Label>
+                      <Label htmlFor="tutorial-toggle">
+                        {t('general:tutorial.enabled')}
+                      </Label>
                       <Switch
                         id="tutorial-toggle"
                         checked={!user?.tutorialsDisabled}
@@ -156,15 +198,18 @@ export default function Settings() {
                       onClick={handleResetTutorials}
                       className="w-full"
                     >
-                      Reset All Tutorials
+                      {t('general:tutorial.reset')}
                     </Button>
                   </div>
                 </CollapsibleContent>
               </Collapsible>
 
-              <Separator />
               <div>
-                <Button className="w-full rounded" onClick={logout}>
+                <Button
+                  className="w-full rounded text-red-500"
+                  onClick={logout}
+                  variant="outline"
+                >
                   <LogIn size={16} transform="rotate(180)" />
                   {t('general:logout')}
                 </Button>
