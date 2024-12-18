bb864d18e8e71e389a4478bb41989efb2735316a
Nils <nils.albin.persson@gmail.com>
2024-12-18 11:22:04 +0100

    Some animations

--- client/src/components/Spinner.tsx
+++ client/src/components/Spinner.tsx
@@ -1,8 +1,13 @@
-import { Loader2 } from "lucide-react";
+import { Loader2 } from 'lucide-react';
 
-export default function Spinner() {
+interface SpinnerProps {
+  label?: string;
+}
+
+export default function Spinner({ label }: SpinnerProps) {
   return (
-    <div className="flex justify-center items-center h-screen absolute top-0 left-0 right-0 bottom-0 bg-black/50">
+    <div className="flex justify-center items-center flex-1 absolute top-0 left-0 right-0 bottom-0 bg-black/50">
+      <p>{label}</p>
       <Loader2 className="animate-spin" size={64} />
     </div>
   );
--- client/src/slides/question/mcqma/Participant.tsx
+++ client/src/slides/question/mcqma/Participant.tsx
@@ -1,6 +1,7 @@
-import { useState } from "react";
-import { MCQMASlide } from "@/models/Quiz";
-import { getColor } from "@/slides/question/base/QuizColors";
+import { useState } from 'react';
+import { MCQMASlide } from '@/models/Quiz';
+import { getColor } from '@/slides/question/base/QuizColors';
+import { motion } from 'framer-motion';
 
 interface Options {
   id: string;
@@ -20,37 +21,38 @@ export function Participant({ slide, answerQuestion }: McqmaViewProps) {
       (prev) =>
         prev.includes(index)
           ? prev.filter((i) => i !== index) // Remove if already selected
-          : [...prev, index], // Add if not selected
+          : [...prev, index] // Add if not selected
     );
   };
 
   const handleSubmit = () => {
     const selectedAnswers = selectedIndexes.map(
-      (index) => slide.options[index].text,
+      (index) => slide.options[index].text
     );
     answerQuestion(selectedAnswers);
   };
 
   return (
-    <div className="flex flex-col items-center justify-center h-full p-10">
+    <div className="flex flex-col items-center justify-center h-full p-10 select-none">
       <h1 className="text-5xl font-display font-bold text-center mb-8 ">
         {slide.title}
       </h1>
       <div className="grid grid-cols-2 gap-6 w-full max-w-3xl">
         {slide.options.map((option: Options, index: number) => (
-          <div
+          <motion.div
             key={option.id}
+            whileTap={{ scale: 0.8 }}
             onClick={() => toggleOption(index)}
             style={{
               backgroundColor: getColor(index),
-              cursor: "pointer",
+              cursor: 'pointer',
             }}
-            className={`flex items-center justify-center text-2xl text-white font-display h-24 rounded-lg  ${
-              selectedIndexes.includes(index) ? "ring-4 ring-white" : ""
+            className={`flex items-center justify-center text-2xl text-white font-display h-24 rounded-lg ${
+              selectedIndexes.includes(index) ? 'ring-4 ring-white' : ''
             }`}
           >
             {option.text}
-          </div>
+          </motion.div>
         ))}
       </div>
       <button
