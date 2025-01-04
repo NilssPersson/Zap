7834917e38dfcb0f38e5befc57a5e72b42422e03
Nils <nils.albin.persson@gmail.com>
2025-01-04 14:59:52 +0100

    File structure changes + Join UI change

--- client/src/pages/JoinQuiz.tsx
+++ client/src/pages/JoinQuiz.tsx
@@ -1,13 +1,13 @@
 import { Input } from '@/components/ui/input';
 import { Button } from '@/components/ui/button';
 import { useState } from 'react';
-import { InfoIcon } from 'lucide-react';
+import { InfoIcon, Play } from 'lucide-react';
 import { ParticipantService } from '@/services/participant';
 import { useNavigate } from 'react-router-dom';
 import { GameShackTitle } from '@/components/GameShackTitle';
 import { Card, CardContent } from '@/components/ui/card';
 
-export default function StartScreen() {
+export default function JoinQuiz() {
   const [quizCode, setQuizCode] = useState('');
   const [showError, setShowError] = useState(false);
 
@@ -49,22 +49,24 @@ export default function StartScreen() {
                 }, 1)
               }
               className={`text-[#333333] text-center font-display py-8 w-full 
-              text-4xl md:text-5xl lg:text-3xl 
+              text-4xl md:text-5xl lg:text-3xl border-2
               ${showError && 'border-red-500 animate-shake'}`}
               value={quizCode}
               onChange={handleInputChange}
             />
             {showError && (
               <div className="flex justify-start items-center w-full text-red-500">
-                <InfoIcon className="w-5 h-5 mr-1 animate-shake" />
+                <InfoIcon className="w-5 h-5 mr-1" />
                 <p className="font-display">Invalid Code</p>
               </div>
             )}
             <Button
               onClick={checkCode}
-              className="bg-[#333333] text-3xl text-[#fefefe] hover:bg-[#86D293] py-8 px-12 font-display w-full"
+              isInteractive
+              className="bg-green-500 text-3xl text-[#fefefe] hover:bg-green-300 py-8 px-12 font-display w-full [&_svg]:!size-5"
             >
               Join Game
+              <Play strokeWidth={4} />
             </Button>
           </CardContent>
         </Card>
