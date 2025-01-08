654c7ad5887d7ee17b599d6c86296609bdb6145f
Nils <nils.albin.persson@gmail.com>
2025-01-08 12:13:57 +0100

    type fix

--- client/src/slides/_components/SlideTitleSpecial.tsx
+++ client/src/slides/_components/SlideTitleSpecial.tsx
@@ -1,11 +1,18 @@
-import { LucideIcon } from "lucide-react";
+import { LucideIcon } from 'lucide-react';
+import { IconType } from 'react-icons/lib';
 
-export default function SlideTitleSpecial({ title, icon }: { title: string, icon: LucideIcon }) {
+export default function SlideTitleSpecial({
+  title,
+  icon,
+}: {
+  title: string;
+  icon: LucideIcon | IconType;
+}) {
   const Icon = icon;
   return (
     <div className="flex flex-row items-center space-x-1">
       <Icon className="w-16 h-16 text-black" />
       <h1 className="text-7xl text-black font-display">{title}</h1>
     </div>
-  )
+  );
 }
