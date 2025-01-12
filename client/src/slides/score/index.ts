5597f63232b88e049facd20bea359c7e6a4f2d19
Nils <nils.albin.persson@gmail.com>
2025-01-12 15:36:31 +0100

    Score slide translation

--- client/src/slides/score/index.ts
+++ client/src/slides/score/index.ts
@@ -14,7 +14,7 @@ export const Info: SlideInfo = {
     icon: BarChart3Icon,
     interactivePreview: true,
     iconColor:"#f472b6",
-    label: "Score Slide",
+    label: "ScoreSlide",
     slideType: SlideTypes.score,
     defaults: {
         title: "Current Standings",
