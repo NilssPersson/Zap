0eeccb79dcfa27f5b2a3df7a59def8b88d97031a
Nils <nils.albin.persson@gmail.com>
2025-01-13 11:22:39 +0100

    Fixed leaving during endscreen and removed statistics until fixed

--- client/src/slides/_specials/endscreen/EndScreen.tsx
+++ client/src/slides/_specials/endscreen/EndScreen.tsx
@@ -4,6 +4,8 @@ import { Quiz, Participant } from '@/models/Quiz';
 import Results from './Results';
 import { useTranslation } from 'react-i18next';
 import Stats from './Stats';
+import { useState } from 'react';
+import { flags } from '@/config/features';
 
 interface EndScreenProps {
   quiz: Quiz;
@@ -11,31 +13,37 @@ interface EndScreenProps {
   participants: Participant[];
 }
 
-export default function EndScreen({
-  endQuiz,
-  participants,
-}: EndScreenProps) {
+export default function EndScreen({ endQuiz, participants }: EndScreenProps) {
   const { t } = useTranslation(['slides']);
+  const [fixedParticipants] = useState(participants);
   return (
     <div className="flex-1 flex flex-col p-20 gap-6">
       <div className="flex-1 flex flex-col items-center mt-4">
-        <Tabs
-          className="flex-1 flex flex-col w-full gap-4"
-          defaultValue="results"
-        >
-          <TabsList className="font-display bg-transparent">
-            <TabsTrigger className="text-6xl" value="results">
-              {t('results')}
-            </TabsTrigger>
-            <TabsTrigger className="text-6xl" value="statistics">{t("statistics")}</TabsTrigger>
-          </TabsList>
-          <TabsContent className="flex-1 overflow-hidden" value="results">
-            <Results participants={participants} />
-          </TabsContent>
-          <TabsContent className="flex-1" value="statistics">
-            <Stats participants={participants} />
-          </TabsContent>
-        </Tabs>
+        {flags.ENABLE_STATISTICS ? (
+          <Tabs
+            className="flex-1 flex flex-col w-full gap-4"
+            defaultValue="results"
+          >
+            <TabsList className="font-display bg-transparent">
+              <TabsTrigger className="text-6xl" value="results">
+                {t('results')}
+              </TabsTrigger>
+              <TabsTrigger className="text-6xl" value="statistics">
+                {t('statistics')}
+              </TabsTrigger>
+            </TabsList>
+            <TabsContent className="flex-1 overflow-hidden" value="results">
+              <Results participants={fixedParticipants} />
+            </TabsContent>
+            <TabsContent className="flex-1" value="statistics">
+              <Stats participants={fixedParticipants} />
+            </TabsContent>
+          </Tabs>
+        ) : (
+          <div className="flex-1 overflow-hidden">
+            <Results participants={fixedParticipants} />
+          </div>
+        )}
       </div>
       <div className="absolute bottom-8 right-10">
         <Button className="max-w-sm " onClick={endQuiz}>
