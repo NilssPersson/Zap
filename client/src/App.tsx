6c4063331bed5b498977eb72d1b99b5c470aeb38
FKnorring <fille.vk@gmail.com>
2025-01-13 14:34:02 +0100

    Moved nosleep component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -6,7 +6,6 @@ import { AppProvider } from './contexts/App/provider';
 import OngoingQuizHandler from './components/OngoingQuizHandler';
 import { TutorialProvider } from './contexts/Tutorial/context';
 import { TutorialOverlay } from './components/tutorial/TutorialOverlay';
-import NoSleepComponent from './NoSleepComponent';
 
 const environment = import.meta.env.VITE_ENVIRONMENT;
 
@@ -19,7 +18,6 @@ function App() {
   return (
     <div className="min-h-dvh inset-0 fixed bg-background bg-[url('/assets/bg-mobile.svg')] md:bg-[url('/assets/bg-desktop.svg')] bg-cover bg-no-repeat bg-center text-white">
       <main className="min-h-dvh max-h-dvh flex flex-col relative">
-        <NoSleepComponent />
         <KindeProvider
           clientId="7b50afa95aec4cba88bad0abb4b94dfa"
           domain="https://gameshack.kinde.com"
@@ -29,7 +27,6 @@ function App() {
           <AppProvider>
             <TutorialProvider>
               <Header />
-
               <AppRoutes />
               <OngoingQuizHandler />
               <Toaster />
